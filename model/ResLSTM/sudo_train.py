f8de71250295964a84725075af458c90384cb17e
HaowenWeiJohn <hwei@wpi.edu>
2021-11-24 10:17:40 -0500

    pesudo train

--- model/ResLSTM/sudo_train.py
+++ model/ResLSTM/sudo_train.py
@@ -18,8 +18,8 @@ sys.path.insert(1, '/work/hwei/HaowenWeiDeepLearning/MOS_Project/AutoDrive_Proje
 
 train_data = np.ones((100, 5, 64, 2048, 10))
 val_data = np.ones((10, 5, 64, 2048, 10))
-train_output = np.ones((100, 64, 2048, 1))
-val_output = np.ones((10, 64, 2048, 1))
+train_output = np.ones((100, 64, 2048, 1), dtype=int)
+val_output = np.ones((10, 64, 2048, 1), dtype=int)
 # prepare sudo data
 
 model = ResLSTM()
